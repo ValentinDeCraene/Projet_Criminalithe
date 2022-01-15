48842007e3811ace48a77be6d113ec57fbaee559
@valentin-VivoBook-ASUSLaptop-X512FA-F512FA <valentin.de.craene@hotmail.fr>
2022-01-15 16:12:35 +0100

    ajout d'un fond à la page d'accueil, ajout d'url de retour aux divers index, corrections typo secondaires

--- Criminalithe/app/constantes.py
+++ Criminalithe/app/constantes.py
@@ -1,7 +1,7 @@
 from warnings import warn
 
 RESULTATS_PAR_PAGES = 5
-SECRET_KEY = "SECRET KEY"
+SECRET_KEY = "L'EPOQUE MODERNE N'EST QU'UN LONG MOYEN AGE"
 
-if SECRET_KEY == "SECRET KEY":
+if SECRET_KEY == "L'EPOQUE MODERNE N'EST QU'UN LONG MOYEN AGE":
     warn("Le secret par défaut n'a pas été changé, vous devriez le faire", Warning)
